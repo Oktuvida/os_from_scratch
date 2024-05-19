3e3f20420fe65bd0f819b2af81d4e800ef9290c4
oktuvida <oktuvida@hotmail.com>
2024-05-19 17:25:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,24 @@ import shutil
 import os
 
 ASM = "nasm"
+CC = "gcc"
+KERNEL_FLAGS = "-Wall -m32 -c -ffreestanding -fno-asynchronous-unwind-tables -fno-pie"
 
 build_dir = "build"
+
 bootstrap_file = os.path.join("src", "bootstrap", "bootstrap.asm")
 bootstrap_output = os.path.join(build_dir, "bootstrap.o")
 
-kernel_file = os.path.join("src", "kernel", "simple_kernel.asm")
-kernel_output = os.path.join(build_dir, "simple_kernel.o")
+init_kernel_files = os.path.join("src", "kernel", "starter.asm")
+init_kernel_output = os.path.join(build_dir, "starter.o")
+
+kernel_file = os.path.join("src", "kernel", "main.c")
+kernel_elf = os.path.join(build_dir, "kernel.elf")
+kernel_bin = os.path.join(build_dir, "kernel.o")
 kernel_image = os.path.join(build_dir, "kernel.img")
 
+linker_file = os.path.join("src", "tools", "linker.ld")
+linker_output = os.path.join(build_dir, "linker.elf")
 
 def main() -> None:
     argv = sys.argv
@@ -31,7 +40,10 @@ def run_func(func: str) -> None:
 def build_kernel() -> None:
     recreate_directory(build_dir)
     run_command(f"{ASM} -f bin {bootstrap_file} -o {bootstrap_output}")
-    run_command(f"{ASM} -f bin {kernel_file} -o {kernel_output}")
+    run_command(f"{ASM} -f elf32 {init_kernel_files} -o {init_kernel_output}")
+    run_command(f"{CC} {KERNEL_FLAGS} {kernel_file} -o {kernel_elf}")
+    run_command(f"ld -melf_i386 -T{linker_file} {init_kernel_output} {kernel_elf} -o {linker_output}")
+    run_command(f"objcopy -O binary {linker_output} {kernel_bin}")
     create_kernel_image()
 
 
@@ -52,9 +64,11 @@ def create_kernel_image() -> None:
 
         img.seek(512)
 
-        with open(kernel_output, "rb") as kf:
-            img.write(kf.read(512 * 5))
+        with open(kernel_bin, "rb") as kb:
+            img.write(kb.read(512 * 5))
 
+        img.seek(512*6)
+        img.write(bytearray(512 * 2046))
 
 def run_command(cmd: str) -> None:
     subprocess.run(cmd.split())
