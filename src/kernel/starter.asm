b9e9696949ba00b405303e9a2160f1e03ea99a02
oktuvida <oktuvida@hotmail.com>
2024-05-29 08:46:57 -0500

    Update remap_pic implementation

--- src/kernel/starter.asm
+++ src/kernel/starter.asm
@@ -1,21 +1,20 @@
-bits 16                       ; Set the assembly mode to 16-bit for the bootloader
+bits 16                       ; Set the assembly mode to 16-bit, suitable for a bootloader
 extern kernel_main            ; Declare an external reference to the kernel's main function
 extern interrupt_handler      ; Declare an external reference to the interrupt handler function
-extern scheduler              ; Special interrupt handler for 32 that calls the scheduler
-extern run_next_process       ; Special interrupt handler for 32 that calls the scheduler
+extern scheduler              ; Declare an external reference to the scheduler function
+extern run_next_process       ; Declare an external reference to the function to run the next process
 
-start:                        ; Label for the start of the bootloader
-    mov ax, cs                ; Move the value of the Code Segment register into AX
-    mov ds, ax                ; Set the Data Segment register to the same value as AX
-        
+start:                        ; Label marking the start of the bootloader
+    mov ax, cs                ; Copy the Code Segment register's value into AX
+    mov ds, ax                ; Set the Data Segment register to AX's value (same as CS)
+
     call load_gdt             ; Call the function to load the Global Descriptor Table
-    call init_video_mode      ; Call the function to initialize the video mode
-    call enter_protected_mode ; Call the function to switch to protected mode
-    call setup_interrupts     ; Call the function to set up interrupts
-	call load_task_register   ; Call the routine that oads the task register with the proper value
+    call init_video_mode      ; Call the function to initialize the video display mode
+    call enter_protected_mode ; Call the function to switch the CPU to protected mode
+    call setup_interrupts     ; Call the function to set up the interrupt handlers
+    call load_task_register   ; Call the function to load the task register with the proper value
 
     call 08h:start_kernel     ; Call the kernel's start function with segment selector 08h
-    
 	
 load_gdt:                     ; Function to load the Global Descriptor Table
     cli                       ; Clear the interrupt flag to disable interrupts
@@ -50,11 +49,54 @@ setup_interrupts:             ; Function to set up interrupts
 remap_pic:                    ; Function to remap the Programmable Interrupt Controller (PIC)
     mov al, 11h               ; Prepare PIC for initialization with ICW1
 	
-    ; Send initialization command to PIC master and slave
-    out 0x20, al              ; Send ICW1 to PIC master command port
-    out 0xa0, al              ; Send ICW1 to PIC slave command port
+	send_init_cmd_to_pic_master: 	
+		out 0x20, al
+		
+	send_init_cmd_to_pic_slave: 	
+		out 0xa0, al
+		
+	; ... ;
 	
-    ; ... ;                   ; Additional commands to configure PIC (omitted for brevity)
+	make_irq_starts_from_intr_32_in_pic_master:		
+		mov al, 32d
+		out 0x21, al
+	
+	make_irq_starts_from_intr_40_in_pic_slave:
+		mov al, 40d
+		out 0xa1, al 
+	
+	; ... ;
+	
+	tell_pic_master_where_pic_slave_is_connected:
+		mov al, 04h
+		out 0x21, al
+	
+	tell_pic_slave_where_pic_master_is_connected:
+		mov al, 02h
+		out 0xa1, al
+	
+	; ... ;
+	
+	mov al, 01h
+	
+	tell_pic_master_the_arch_is_x86:
+		out 0x21, al
+	
+	tell_pic_slave_the_arch_is_x86:
+		out 0xa1, al
+	
+	; ... ;
+	
+	mov al, 0h
+	
+	make_pic_master_enables_all_irqs:
+		out 0x21, al
+	
+	make_pic_slave_enables_all_irqs:
+		out 0xa1, al
+	
+	; ... ;
+
 	
     ret                       ; Return from the function
 	
@@ -68,24 +110,24 @@ load_task_register:
     ltr ax
     
     ret
-	
-bits 32                       ; Set the assembly mode to 32-bit for the kernel
-start_kernel:                 ; Label for the start of the kernel
-    mov eax, 10h              ; Move the value 10h into EAX
-    mov ds, eax               ; Set the Data Segment register to 10h
-    mov ss, eax               ; Set the Stack Segment register to 10h
-    
-    mov eax, 0h               ; Move the value 0h into EAX
-    mov es, eax               ; Set the Extra Segment register to 0h
-    mov fs, eax               ; Set the FS register to 0h
-    mov gs, eax               ; Set the GS register to 0h
-	
-    sti                       ; Set the interrupt flag to enable interrupts
-	
-    call kernel_main          ; Call the kernel's main function
-	
-%include "gdt.asm" ; Include the contents of the file "gdt.asm"
-%include "idt.asm" ; Include the contents of the file "idt.asm"
 
-tss:
-    dd 0
+bits 32                       ; Switch to 32-bit assembly mode, suitable for the kernel
+start_kernel:                 ; Label marking the start of the kernel
+    mov eax, 10h              ; Set EAX to 10h, which is the selector for the data segment
+    mov ds, eax               ; Set the Data Segment register to EAX's value
+    mov ss, eax               ; Set the Stack Segment register to EAX's value
+
+    mov eax, 0h               ; Clear EAX to zero
+    mov es, eax               ; Set the Extra Segment register to zero
+    mov fs, eax               ; Set the FS segment register to zero
+    mov gs, eax               ; Set the GS segment register to zero
+
+    sti                       ; Enable interrupts by setting the interrupt flag
+
+    call kernel_main          ; Call the kernel's main function
+
+%include "gdt.asm"            ; Include the Global Descriptor Table definitions
+%include "idt.asm"            ; Include the Interrupt Descriptor Table definitions
+
+tss:                          ; Task State Segment definition
+    dd 0                      ; Initialize the TSS with zero
